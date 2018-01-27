631976cc94b2b2dd12b065decab7f3b2bceffb0a
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-26 19:46:22 -0800

    About to write network adapters for meetup

---
+++ MeetEvent-App/Networking Files/NetworkAdapter.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkAdapter.swift
+//  MeetEvent-App
+//
+//  Created by Matthew Harrilal on 1/26/18.
+//  Copyright © 2018 Yveslym. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Moya
+
