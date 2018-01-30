f05bedd4a85cf3738d0e1794ee55a06831679b76
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-29 20:00:39 -0800

    Implemented log in and sign up functionality

--- Server-Files/CustomClass.py
+++ Server-Files/CustomClass.py
@@ -6,4 +6,4 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
-return json.JSONEncoder.default(self, o)
+        return json.JSONEncoder.default(self, o)
--- Server-Files/server.py
+++ Server-Files/server.py
@@ -13,3 +13,105 @@ from basicauth import decode
 from bson.json_util import dumps
 
 app = Flask(__name__)
+
+
+client = MongoClient('mongodb://matthewHarrilal:matthewharrilal1@ds119088.mlab.com:19088/meet_event_development')
+app.db = client.meet_event_development
+database = app.db
+rounds = app.bcrypt_rounds = 5
+api = Api(app)
+users_collection = database.users_collection
+
+def authenticated_request(func):
+    def wrapper(*args, **kwargs):
+        auth = request.authorization
+        print('***********')
+        print(request.authorization)
+        print('***********')
+        auth_code = request.headers['authorization']
+        email,password = decode(auth_code)
+
+        if email is not None and password is not None:
+            user_collection = database.orchard_collection
+            user = user_collection.find_one({'email': email})
+            if user is not None:
+                encoded_password = password.encode('utf-8')
+                if bcrypt.checkpw(encoded_password, user['password']):
+                    return func(*args, **kwargs)
+                else:
+                    return ({'error': 'email or password is not correct'}, 401, None)
+            else:
+                return ({'error': 'could not find user in the database'}, 400, None)
+        else:
+            return ({'error': 'enter both email and password'}, 400, None)
+
+    return wrapper
+
+
+class User(Resource):
+
+    # The decorator makes authenticated requests
+    @authenticated_request
+    def get(self):
+        '''This is the log in function that is why we need the headers'''
+
+        # Accessing the headers becuase that is where the users credentials are passed
+        auth = request.authorization
+
+        # So now that we have the credentials we have to see if the user exists
+        user_find = user_collection.find_one({'email': auth.username})
+
+        # Now that I have searched for them I have to see if they exist
+        if user_find is not None:
+            user_find.pop('password')
+            print("The user has been succesfully fetched")
+            return(user_find, 200, None)
+
+    def post(self):
+        # This function sends the user to the database 
+
+        requested_json = request.json
+
+        # We access headers headers through request.authorization and the body through request.json
+        requested_password = request.json['password']
+
+        # Encodes password into a sanitized format
+        encoded_password = requested_password.encode("utf-8")
+
+        # Based on the number of rounds we give it we run the encrpytion algorithm
+        # against the encoded password
+        hashed_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt(rounds))
+
+        # So updating value of old string password with new hashed password
+        requested_password = hashed_password
+
+        # Make sure users give us email and password
+        if 'email' in requested_json and 'password' in requested_json:
+            users_collection.insert_one(requested_json)
+            requested_json.pop('password')
+            print("User has succesfully been posted")
+            return(requested_json, 201, None)
+
+
+
+
+
+
+    
+
+    
+api.add_resource(User, "/users")
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(JSONEncoder().encode(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+# Encodes our resouces for
+
+
+
+if __name__ == '__main__':
+    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(debug=True)
